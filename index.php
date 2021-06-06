ad1ebeea0b4db356199a1194867cf18360fdd736
KarloKocisNS <kocis.karlo91@gmail.com>
2021-06-06 03:01:45 +0200

    latest file

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatibile" content="IE-edge">
+        <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    </head>
+  <title>Formapp</title>
+  <center>
+  <div class="parent">
+  <center><h1 style="color: grey" >PHP FORM</h1></center>
+<center>
+     <table>
+        <tr>
+        <td>Name*<br><input type: text></input></td>
+        </tr>
+        <tr>
+        <td>Lastname*<br><input type: text></input></td>
+        </tr>
+        <tr>
+        <td>E-mail*<br><input type: text></input></td>
+        </tr>
+     </table>
+     
+    <div class="btn1">
+     <th><input class="btn btn-outline-primary" type="button" value="INSERT"></th>
+     <th><input class="btn btn-outline-success" type="button" value="SELECT"></th>
+     <th><input class="btn btn-outline-danger" type="button" value="DELETE"></th>
+     </div>
+</center>
+    </div>
+</center>
+
+<body>
+
+</body>
+
+</html>
